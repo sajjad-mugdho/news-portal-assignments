421687c592b20bbaa2d32f4f85132f5ac0bc7526
sajjad-mugdho <sajjadmugdho@yahoo.com>
2022-09-03 16:54:38 +0600

    java start

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('bismillah');
+// loadCategory 
+const loadCategory = async () => {
+    const url = "https://openapi.programming-hero.com/api/news/categories"
+    try {
+        const res = await fetch(url)
+        const data = await res.json()
+        displayCategory(data.data.news_category)
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
